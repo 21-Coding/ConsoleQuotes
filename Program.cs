fae1a39975d0c32d39ff96c498370b57541ff3fd
Dom Youmans <ddyoumans@icloud.com>
2020-08-30 22:37:08 -0700

    update quotes for Kobe.

--- Program.cs
+++ Program.cs
@@ -8,12 +8,19 @@ namespace myApp
      static void Main(string[] args)
     {
 
-      TypeLine("Hello, to see your favorite Nipsey Hussle & Kobe Bryant quotes please type a number from 1 - 48!");
+      TypeLine("Hello, to see your favorite Nipsey Hussle & Kobe Bryant quotes please type a number from 1 - 50!");
       string stringUserNumber = Console.ReadLine();
       int intUserNumber = int.Parse(stringUserNumber);
 
+      
 
-       if (intUserNumber == 49)
+      if (intUserNumber == 50)
+      {
+        Console.ForegroundColor = ConsoleColor.DarkMagenta;
+        Console.BackgroundColor = ConsoleColor.DarkYellow;
+        TypeLine("I focus on one thing and one thing only.");
+      }
+      else if (intUserNumber == 49)
       {
         Console.ForegroundColor = ConsoleColor.DarkMagenta;
         Console.BackgroundColor = ConsoleColor.DarkYellow;
@@ -308,7 +315,7 @@ namespace myApp
         TypeLine("It's possible to monetize your art without compromising the integrity of it for commerce.");
       }
       else {
-        TypeLine("Invalid input, try the numbers 1-48.");
+        TypeLine("Invalid input, try the numbers 1-50.");
       }
 
         }
