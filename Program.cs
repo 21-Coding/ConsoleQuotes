628e06d6363f39d5a0f67f0e8b27d2ada43f0fb7
Dom Youmans <ddyoumans@icloud.com>
2020-08-31 22:47:11 -0700

    add new error message and greeting.

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace myApp
      static void Main(string[] args)
     {
 
-      TypeLine("Hello, to see your favorite Nipsey Hussle & Kobe Bryant quotes please type a number from 1 - 50!");
+      TypeLine("Hello, to see your favorite Nipsey Hussle & Kobe Bryant quotes please type a number from 1 - 51!");
       string stringUserNumber = Console.ReadLine();
       int intUserNumber = int.Parse(stringUserNumber);
 
@@ -320,7 +320,7 @@ namespace myApp
         TypeLine("It's possible to monetize your art without compromising the integrity of it for commerce.");
       }
       else {
-        TypeLine("Invalid input, try the numbers 1-50.");
+        TypeLine("Invalid input, try the numbers 1-51.");
       }
 
         }
