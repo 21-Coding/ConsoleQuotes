543788969fc50268ef27c62315d87427e97887a4
Dom Youmans <ddyoumans@icloud.com>
2020-09-04 23:17:59 -0700

    adjust alerts.

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace myApp
      static void Main(string[] args)
     {
 
-      TypeLine("Hello, to see your favorite Nipsey Hussle & Kobe Bryant quotes please type a number from 1 - 54!");
+      TypeLine("Hello, to see your favorite Nipsey Hussle & Kobe Bryant quotes please type a number from 1 - 55!");
       string stringUserNumber = Console.ReadLine();
       int intUserNumber = int.Parse(stringUserNumber);
 
@@ -340,7 +340,7 @@ namespace myApp
         TypeLine("It's possible to monetize your art without compromising the integrity of it for commerce.");
       }
       else {
-        TypeLine("Invalid input, try the numbers 1-54.");
+        TypeLine("Invalid input, try the numbers 1-55.");
       }
 
         }
