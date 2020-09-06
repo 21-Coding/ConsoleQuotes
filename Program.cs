4e3f396f0216ea33d2ab6dfc2fa6d0b1e9e593b8
Dom Youmans <ddyoumans@icloud.com>
2020-09-05 23:59:10 -0700

    add new alert message.

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace myApp
      static void Main(string[] args)
     {
 
-      TypeLine("Hello, to see your favorite Nipsey Hussle & Kobe Bryant quotes please type a number from 1 - 64!");
+      TypeLine("Hello, to see your favorite Nipsey Hussle & Kobe Bryant quotes please type a number from 1 - 65!");
       string stringUserNumber = Console.ReadLine();
       int intUserNumber = int.Parse(stringUserNumber);
 
