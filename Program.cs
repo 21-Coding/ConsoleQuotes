21c4a23779d932155342cbbd233f87fa0b869c86
Dom Youmans <ddyoumans@icloud.com>
2020-09-12 23:12:00 -0700

    add new message.

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace myApp
      static void Main(string[] args)
     {
 
-      TypeLine("Hello, to see your favorite Nipsey Hussle & Kobe Bryant quotes please type a number from 1 - 76!");
+      TypeLine("Hello, to see your favorite Nipsey Hussle & Kobe Bryant quotes please type a number from 1 - 77!");
       string stringUserNumber = Console.ReadLine();
       int intUserNumber = int.Parse(stringUserNumber);
 
@@ -450,7 +450,7 @@ namespace myApp
         TypeLine("It's possible to monetize your art without compromising the integrity of it for commerce.");
       }
       else {
-        TypeLine("Invalid input, try the numbers 1-76.");
+        TypeLine("Invalid input, try the numbers 1-77.");
       }
 
         }
